2e3effe4a0415b08e00b323c8ae493de7a468527
˹ɴᴏᴛᴛʏ ꭙ˼ <124904806+NottyX@users.noreply.github.com>
2023-02-10 15:38:19 +0530

    ˹ɴσᴛᴛʏ ꭙ˼

--- config/config.py
+++ config/config.py
@@ -31,7 +31,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001572595035"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", NOTTY MUSIC BOT")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Notty Music Bot")
 
 # Your User ID.
 OWNER_ID = list(
